afef11f367477f2cd26ead0969ee5fb21de13068
Kazuki Yoneda <touchesthewalls@i.softbank.jp>
2015-12-22 00:41:12 +0900

    出力可能

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,5 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -10,95 +9,88 @@ import javax.swing.JScrollBar;
 import javax.swing.JSeparator;
 import java.awt.Panel;
 import java.awt.Canvas;
+import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
-
 public class GUI extends JFrame {
+private JPanel contentPane;
+Graphical_sample start_arrange;
+/**
+* Launch the application.
+*/
+public static void main(String[] args) {
+EventQueue.invokeLater(new Runnable() {
+public void run() {
+try {
+GUI frame = new GUI();
+frame.setVisible(true);
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
+});
+}
+/**
+* Create the frame.
+*/
+String a;
+public GUI()
+{
+setTitle("Planner");
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setBounds(100, 100, 1200, 800);
+contentPane = new JPanel();
+contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+setContentPane(contentPane);
+contentPane.setBackground(Color.gray);
+contentPane.setLayout(null);
+JLabel lblStart = new JLabel("Start");
+lblStart.setBounds(10, 12, 61, 15);
+contentPane.add(lblStart);
+float graphical_scale = 0.5f;
+start_arrange = new Graphical_sample(graphical_scale);
+start_arrange.setBounds(10, 40, (int)(800*graphical_scale), (int)(608*graphical_scale));
+contentPane.add(start_arrange);
+JLabel lblFinish = new JLabel("Finish");
+lblFinish.setBounds(10, 420, 61, 15);
+contentPane.add(lblFinish);
+Graphical_sample goal_arrange = new Graphical_sample(graphical_scale);
+goal_arrange.setBounds(10, 440, (int)(800*graphical_scale), (int)(608*graphical_scale));
+contentPane.add(goal_arrange);
+JScrollPane scrollPane_2 = new JScrollPane();
+scrollPane_2.setBounds(820, 40, 170, 179);
+contentPane.add(scrollPane_2);
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI frame = new GUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
+final JTextArea area = new JTextArea();
+scrollPane_2.setViewportView(area);
+JLabel lblProcess = new JLabel("Process");
+lblProcess.setBounds(820, 12, 61, 15);
+contentPane.add(lblProcess);
+JButton btnRun = new JButton("Run");
+btnRun.setBounds(64, 380, 80, 25);
+btnRun.addActionListener(
+		new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				area.append("aaa");
+				for(String s :start_arrange.getCurrentState()){
+					System.out.println(s);
+					}
+					Planner planner = new Planner();
+					}
+			
+			
 		});
-	}
+contentPane.add(btnRun);
+JButton btnBack = new JButton("Back");
+btnBack.setBounds(900, 234, 80, 25);
+contentPane.add(btnBack);
+JButton btnGo = new JButton("Go");
+btnGo.setBounds(960, 234, 80, 25);
+contentPane.add(btnGo);
+}
 
-	/**
-	 * Create the frame.
-	 */
-	public GUI() {
-		setTitle("Planner");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblStart = new JLabel("Start");
-		lblStart.setBounds(36, 12, 61, 15);
-		contentPane.add(lblStart);
-		
-		JLabel lblFinish = new JLabel("Finish");
-		lblFinish.setBounds(36, 123, 61, 15);
-		contentPane.add(lblFinish);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(25, 39, 165, 72);
-		contentPane.add(scrollPane);
-		
-		JTree tree = new JTree();
-		scrollPane.setViewportView(tree);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(25, 150, 165, 72);
-		contentPane.add(scrollPane_1);
-		
-		JTree tree_1 = new JTree();
-		scrollPane_1.setViewportView(tree_1);
-		
-		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBounds(243, 40, 170, 179);
-		contentPane.add(scrollPane_2);
-		
-		JTree tree_2 = new JTree();
-		scrollPane_2.setViewportView(tree_2);
-		
-		JLabel lblProcess = new JLabel("Process");
-		lblProcess.setBounds(253, 12, 61, 15);
-		contentPane.add(lblProcess);
-		
-		JButton btnRun = new JButton("Run");
-		btnRun.setBounds(64, 234, 80, 25);
-		contentPane.add(btnRun);
-		
-		JButton btnBack = new JButton("Back");
-		btnBack.setBounds(244, 234, 80, 25);
-		contentPane.add(btnBack);
-		
-		JButton btnGo = new JButton("Go");
-		btnGo.setBounds(333, 234, 80, 25);
-		contentPane.add(btnGo);
-		
-	}
-	public void actionPerformed(ActionEvent e){
-	    String cmd = e.getActionCommand();
-	    if(cmd.equals("Run")){
-	    	Planner planner = new Planner();
-	    }
-	}
 }
--- src/Graphical_sample.java
+++ src/Graphical_sample.java
@@ -1,174 +1,173 @@
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.HashMap;
-
-public class Graphical_sample extends JFrame
+public class Graphical_sample extends JPanel
 {
-	public static void main(String args[]){
-		Graphical_sample frame = new Graphical_sample("タイトル");
-		frame.setVisible(true);
-	}
-
-	HashMap<String,JLabel> blocks = new HashMap<String,JLabel>();
-	HashMap<PosName,Position> positions = new HashMap<PosName,Position>();
-
-	public enum State {
-		FILL, EMPTY, DISABLE 
-	}
-	public enum PosName{
-		left_bottom, left_middle, left_top,
-		center_bottom, center_middle, right_bottom, arm
-	}
-	public class Position
-	{
-		public int x;
-		public int y;
-		public State state;
-		public JLabel label;
-		public Position(int X, int Y)
-		{
-			x = X;
-			y = Y;
-			state = State.DISABLE;
-			label = GenerateLabel("empty.png",x, y, 128, 128);
-//			new Timer(30,(ActionListener) label).start(); 
-			//イベントリスナー定義してタイマーオブジェクトをいじる
-		}
-	}
-
-	Graphical_sample(String title){
-		setTitle(title);
-		setBounds(0, 0, 800, 608);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JPanel p = new JPanel();
-		p.setLayout(null);
-
-		positions.put(PosName.left_bottom,new Position(64, 352));
-		positions.put(PosName.left_middle,new Position(64, 224));
-		positions.put(PosName.left_top,new Position(64, 96));
-		positions.put(PosName.center_bottom,new Position(256, 352));
-		positions.put(PosName.center_middle,new Position(256, 224));
-		positions.put(PosName.right_bottom,new Position(448, 352));
-		positions.put(PosName.arm,new Position(448, 64));
-
-		//ブロック
-		SetBlock(GenerateLabel("block_a.png",650, 64, 128, 128),p);
-		SetBlock(GenerateLabel("block_b.png",650, 256, 128, 128),p);
-		SetBlock(GenerateLabel("block_c.png",650, 448, 128, 128),p);
-
-		//固定パーツ
-		JLabel arm = GenerateLabel("arm.png",438, 0, 148, 128);
-		p.add(arm);
-		JLabel floor = GenerateLabel("floor.png",0, 480, 640, 128);
-		p.add(floor);
-
-		//点線ラベルをパネルに追加
-		for (PosName n : PosName.values()) {
-			p.add(positions.get(n).label);
-		}
-
-		//非表示
-		//empty_floor3.setVisible(false);
-
-		Container contentPane = getContentPane();
-		contentPane.add(p, BorderLayout.CENTER);
-	}
-
-	private JLabel GenerateLabel(String imgName, int x, int y, int witdh, int height)
-	{
-		ImageIcon icon = new ImageIcon("./img/"+imgName);
-		JLabel label = new JLabel(icon);
-		label.setBounds(x, y, witdh, height);
-		return label;
-	}
-
-	private void SetBlock(JLabel label,JPanel p)
-	{
-		blocks.put(label.getIcon().toString(), label);
-
-		// リスナーを登録
-		MyMouseListener listener = new MyMouseListener(label.getIcon().toString());
-		label.addMouseListener(listener);
-		label.addMouseMotionListener(listener);
-
-		p.add(label);
-	}
-
-	private static class Validator {
-		public static boolean includes(int lower, int upper, int value) {
-			return lower <= value && value <= upper;
-		}
-	}
-
-	private class MyMouseListener extends MouseAdapter
-	{
-		private int dx;
-		private int dy;
-		private int init_x;
-		private int init_y;
-		private String key;
-
-//		public void actionPerformed(ActionEvent e){
-//			System.out.println("aaa");
-//		} 
-		
-		public MyMouseListener(String key)
-		{
-			this.key = key;
-		}
-
-		public void mouseDragged(MouseEvent e) 
-		{
-			// マウスの座標からラベルの左上の座標を取得する
-			int x = e.getXOnScreen() - dx;
-			int y = e.getYOnScreen() - dy;
-			blocks.get(key).setLocation(x, y);
-		}
-
-		public void mousePressed(MouseEvent e) 
-		{
-			// 押さえたところからラベルの左上の差を取っておく
-			init_x = blocks.get(key).getX();
-			init_y = blocks.get(key).getY();
-			dx = e.getXOnScreen() - init_x;
-			dy = e.getYOnScreen() - init_y;
-		}
-
-		public void mouseReleased(MouseEvent e) 
-		{
-			int x = e.getXOnScreen() - dx;
-			int y = e.getYOnScreen() - dy;
-
-			if(IsFit(x, y, 448, 64)){return;}
-
-			if(IsFit(x, y, 64, 352)){return;}
-			if(IsFit(x, y, 256, 352)){return;}
-			if(IsFit(x, y, 448, 352)){return;}
-
-			if(IsFit(x, y, 64, 224)){return;}
-			if(IsFit(x, y, 64, 96)){return;}
-			if(IsFit(x, y, 256, 224)){return;}
-
-			//可動領域でなければ初期位置に戻す
-			blocks.get(key).setLocation(init_x, init_y);
-		}
-
-		private boolean IsFit(int x,int y,int fit_x,int fit_y)
-		{
-			//80 = 64(半径) + 12(遊び)
-			if(Validator.includes(fit_x-80, fit_x+80, x)
-					&& Validator.includes(fit_y-80, fit_y+80, y)){
-				blocks.get(key).setLocation(fit_x, fit_y);
-				return true;
-			}
-			return false;
-		}
-
-	}
+private static float scale;
+public static void main(String args[])
+{
+Graphical_sample aaa = new Graphical_sample(0.5f);
+JFrame jframe = new JFrame("DrawRect");
+Container c = jframe.getContentPane();
+c.add(aaa, BorderLayout.CENTER);
+jframe.setSize((int)(800*scale), (int)(608*scale));
+jframe.setVisible(true);
+}
+HashMap<String,JLabel> blocks = new HashMap<String,JLabel>();
+HashMap<PosName,Position> positions = new HashMap<PosName,Position>();
+public enum State {
+FILL, EMPTY, DISABLE
+}
+public enum PosName{
+left_bottom, left_middle, left_top,
+center_bottom, center_middle, right_bottom, arm
+}
+public class Position
+{
+public int x;
+public int y;
+public State state;
+public JLabel label;
+//stateがFillの時のみ利用。それ以外の場合は不定とする。
+public String blockId;
+public Position(int X, int Y,JPanel p)
+{
+x = X;
+y = Y;
+state = State.DISABLE;
+label = GenerateLabel("empty.png",x, y, 128, 128);
+p.add(label);
+// new Timer(30,(ActionListener) label).start();
+//TODO: イベントリスナー定義してタイマーオブジェクトをいじる
+}
+}
+Graphical_sample(float scale){
+this.scale = scale;
+this.setLayout(null);
+setBounds(0, 0, 800, 608);
+positions.put(PosName.arm, new Position(448, 64,this));
+positions.put(PosName.left_bottom, new Position(64, 352,this));
+positions.put(PosName.left_middle, new Position(64, 224,this));
+positions.put(PosName.left_top, new Position(64, 96,this));
+positions.put(PosName.center_bottom,new Position(256, 352,this));
+positions.put(PosName.center_middle,new Position(256, 224,this));
+positions.put(PosName.right_bottom, new Position(448, 352,this));
+//ブロック
+SetBlock(GenerateLabel("block_a.png",650, 64, 128, 128),this);
+SetBlock(GenerateLabel("block_b.png",650, 256, 128, 128),this);
+SetBlock(GenerateLabel("block_c.png",650, 448, 128, 128),this);
+//固定パーツ
+JLabel arm = GenerateLabel("arm.png",438, 0, 148, 128);
+this.add(arm);
+JLabel floor = GenerateLabel("floor.png",0, 480, 640, 128);
+this.add(floor);
+//点線ラベルをパネルに追加
+for (PosName n : PosName.values()) {
+this.add(positions.get(n).label);
+}
+}
+private JLabel GenerateLabel(String imgName, int x, int y, int witdh, int height)
+{
+ImageIcon icon = new ImageIcon("./img/"+imgName);
+MediaTracker tracker = new MediaTracker(this);
+// ポイント２．getScaledInstanceで大きさを変更します。
+Image smallImg = icon.getImage().getScaledInstance((int) (icon.getIconWidth() * scale), -1,
+Image.SCALE_SMOOTH);
+// ポイント３．MediaTrackerで処理の終了を待ちます。
+tracker.addImage(smallImg, 1);
+ImageIcon smallIcon = new ImageIcon(smallImg);
+JLabel label = new JLabel(smallIcon);
+label.setBounds((int)(x*scale), (int)(y*scale), (int)(witdh*scale), (int)(height*scale));
+return label;
+}
+private void SetBlock(JLabel label,JPanel p)
+{
+blocks.put(label.getIcon().toString(), label);
+// リスナーを登録
+MyMouseListener listener = new MyMouseListener(label.getIcon().toString());
+label.addMouseListener(listener);
+label.addMouseMotionListener(listener);
+p.add(label);
+}
+private static class Validator {
+public static boolean includes(int lower, int upper, int value) {
+return lower <= value && value <= upper;
+}
+}
+public ArrayList<String> getCurrentState(){
+ArrayList<String> initialState = new ArrayList<String>();
+return initialState;
+}
+private class MyMouseListener extends MouseAdapter
+{
+private int dx;
+private int dy;
+private int init_x;
+private int init_y;
+private String key;
+public MyMouseListener(String key)
+{
+this.key = key;
+}
+public void mouseDragged(MouseEvent e)
+{
+// マウスの座標からラベルの左上の座標を取得する
+int x = e.getXOnScreen() - dx;
+int y = e.getYOnScreen() - dy;
+blocks.get(key).setLocation(x, y);
+}
+public void mousePressed(MouseEvent e)
+{
+// 押さえたところからラベルの左上の差を取っておく
+init_x = blocks.get(key).getX();
+init_y = blocks.get(key).getY();
+dx = e.getXOnScreen() - init_x;
+dy = e.getYOnScreen() - init_y;
+}
+public void mouseReleased(MouseEvent e)
+{
+int x = e.getXOnScreen() - dx;
+int y = e.getYOnScreen() - dy;
+//各ポジションの判定
+for (PosName n : PosName.values()) {
+//判定されなければ続行
+if(!IsFit(x, y, n)){
+continue;
+}
+//判定された場合
+//前状態を削除
+for (PosName pn : PosName.values()) {
+Position pos = positions.get(pn);
+if(pos.state.equals(State.FILL) && pos.blockId.equals(key)){
+positions.get(pn).state = State.EMPTY;
+break;
+}
+}
+//現在状態をセット
+Position pos = positions.get(n);
+pos.state = State.FILL;
+pos.blockId = key;
+return;
+}
+//可動領域でなければ初期位置に戻す
+blocks.get(key).setLocation(init_x, init_y);
+}
+private boolean IsFit(int x,int y, PosName posName)
+{
+Position pos = positions.get(posName);
+//80 = 64(半径) + 12(遊び)
+int range = 80;
+if(Validator.includes(pos.x-range, pos.x+range, (int)(x/scale))
+&& Validator.includes(pos.y-range, pos.y+range, (int)(y/scale))){
+blocks.get(key).setLocation((int)(pos.x*scale), (int)(pos.y*scale));
+return true;
+}
+return false;
+}
+}
 }
